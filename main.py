5319147defdad723b45572abb2f6aab0245c7e61
jara <jaroslava.bianko@gmail.com>
2025-03-04 10:56:14 +0000

    Add Person class

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
-# change it to print your name
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
 
-print("Hello, I am Piotr Brudny")
+    def __str__(self):
+        return f"{self.name}({self.age})"
+
+    def myfunc(self):
+        print("Hello my name is " + self.name)
+
+# change it to print your name
+print("Hello, I am Yaraslava Bianko")

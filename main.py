559e37d0e5716efc5b86ac3cc2178a5fec99eae1
jara <jaroslava.bianko@gmail.com>
2025-03-06 13:39:34 +0000

    Add Person class

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 class Person:
-    def __init__(self, name, age):
+    def __init__(self, name, age, address):
         self.name = name
         self.age = age
+        self.address = address
 
     def __str__(self):
-        return f"{self.name}({self.age})"
+        return f"{self.name}({self.age}) lives at {self.address}"
 
     def myfunc(self):
-        print("Hello my name is " + self.name)
+        print("Hello my name is " + self.name + "! I live at " + self.address)
 
-p1 = Person("Yaraslava Bianko", 18)
+p1 = Person("Yaraslava Bianko", 18, "[insert some address]")
 print(p1)
 p1.myfunc()

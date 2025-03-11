7bc2449599a8a271d2be2fbe006fa25213568dae
jara <jaroslava.bianko@gmail.com>
2025-03-11 11:13:47 +0100

    Create basic_data_structures.py
    Assign data structures to variables

---
+++ basic_data_structures.py
@@ -0,0 +1,8 @@
+num = 3 #int
+num1 = 3.1 #float
+s = "hello" #string
+list1 = [1, 1.2, "string", ["another list", 3]] #list
+dict1 = {"key1" : 1, "key2" : "hi"} #dict
+tuple1 = (1, 2, 3) #tup
+set1 = {"a", "b"} #set
+night_time = False #bool

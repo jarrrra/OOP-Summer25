5265abe41667ea8fa6b692e7a2bb2fe0680ae478
jara <jaroslava.bianko@gmail.com>
2025-03-13 14:20:31 +0100

    Add Student Details Structure

---
+++ student_details_structure.py
@@ -0,0 +1,10 @@
+student1 = {
+  'first_name' : 'John',
+  'last_name' : 'Doe',
+  'index_number' : 10013,
+  'nationality' : 'undisclosed',
+  'starting_date' : 'September 1st',
+  'courses' : ['Math', 'Economics'] 
+}
+
+print(student1.first_name, ' ', student1.last_name, ' ', student1.index_number)

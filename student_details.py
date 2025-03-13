f246947c57cffdfc9374193f6c17a8e9c22db3a6
jara <jaroslava.bianko@gmail.com>
2025-03-13 14:15:18 +0100

    Add Student Details

---
+++ student_details.py
@@ -0,0 +1,6 @@
+first_name = 'John'
+last_name = 'Doe'
+index_number = 10013
+nationality = 'undisclosed'
+starting_date = 'September 1st'
+courses = ['Math', 'Economics'] #for the sake of variable variety, they can have multiple courses

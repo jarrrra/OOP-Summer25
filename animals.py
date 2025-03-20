b18bb05f8a49f919ba5f7cfd44ab11469f427042
jara <jaroslava.bianko@gmail.com>
2025-03-20 14:25:24 +0100

    Create Animals dictionaries

---
+++ animals.py
@@ -0,0 +1,30 @@
+cat = {
+  'name' : 'cat',
+  'group' : 'mammal',
+  'number_of_legs' : 4,
+  'skills' : ['meowing', 'hunting', 'zoomies']
+}
+dog = {
+  'name' : 'dog',
+  'group' : 'mammal',
+  'number_of_legs' : 4,
+  'skills' : ['barking', 'fetching sticks', 'tail wagging']
+}
+crow = {
+  'name' : 'crow',
+  'group' : 'bird',
+  'number_of_legs' : 2,
+  'skills' : ['flying', 'copying sounds', 'jumping around']
+}
+mouse = {
+  'name' : 'mouse',
+  'group' : 'mammal',
+  'number_of_legs' : 4,
+  'skills' : ['squeaking']
+}
+gecko = {
+  'name' : 'gecko',
+  'group' : 'reptile',
+  'number_of_legs' : 4,
+  'skills' : ['can loose its tail']
+}
